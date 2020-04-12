f24fe476ffcbcab5828491b66100cf79ec69371b
antoine prudhomme <antoine.prudhomme@spendesk.com>
2020-04-12 17:51:06 +0200

    extract data from all countries

---
+++ colorsChart.js
@@ -0,0 +1,8 @@
+const colorsChart = {
+  red: "rgb(213, 0, 0)",
+  redLight: "rgb(255, 138, 128)",
+  green: "rgb(0, 230, 118)",
+  greenLight: "rgb(185, 246, 202)",
+  orange: "rgb(255, 145, 0)",
+  orangeLight: "rgb(255, 209, 128)",
+};
--- data/extractData.js
+++ data/extractData.js
@@ -1,29 +1,46 @@
-const fs = require('fs');
-const allData = require('./coronavirus.politologue.com-data-2020-04-11.json');
+const fs = require("fs");
+const allData = require("./coronavirus.politologue.com-data-2020-04-11.json");
 
-const frenchData = allData.PaysData
-  .filter((data) => data.Pays === 'France')
-  .map((data) => {
-    return {
-      date: data.Date,
-      country: data.Pays,
-      infections: data.Infection,
-      deaths: data.Deces,
-      healings: data.Guerisons,
-    };
-  })
-  .sort((a, b) => new Date(a.date) - new Date(b.date));
+const dataByCountryMap = new Map();
 
-const globalData = allData.GlobalData
-  .map((data) => {
-    return {
-      date: data.Date,
-      infections: data.Infection,
-      deaths: data.Deces,
-      healings: data.Guerisons,
-    };
-  })
-  .sort((a, b) => new Date(a.date) - new Date(b.date));
+const globalData = allData.GlobalData.map((data) => {
+  return {
+    date: data.Date,
+    infections: data.Infection,
+    deaths: data.Deces,
+    healings: data.Guerisons,
+  };
+});
+dataByCountryMap.set("global", globalData);
 
-fs.writeFileSync('./data/french.json', JSON.stringify(frenchData));
-fs.writeFileSync('./data/global.json', JSON.stringify(globalData));
+allData.PaysData.forEach((data) => {
+  if (!dataByCountryMap.has(data.Pays)) {
+    dataByCountryMap.set(data.Pays, []);
+  }
+  dataByCountryMap.get(data.Pays).push({
+    date: data.Date,
+    country: data.Pays,
+    infections: data.Infection,
+    deaths: data.Deces,
+    healings: data.Guerisons,
+  });
+});
+
+Array.from(dataByCountryMap.keys()).forEach((key) => {
+  dataByCountryMap.set(
+    key,
+    dataByCountryMap
+      .get(key)
+      .sort((a, b) => new Date(a.date) - new Date(b.date))
+  );
+});
+
+const dataByCountryObject = {};
+dataByCountryMap.forEach((value, key) => {
+  dataByCountryObject[key] = value;
+});
+
+fs.writeFileSync(
+  "./data/all.js",
+  `const dataByCountryJSON = \`${JSON.stringify(dataByCountryObject)}\`;`
+);
